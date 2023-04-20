c4e497e95a01c671c5d61918f2ce747f95d41b00
Zeke-White <107807694+Zeke-White@users.noreply.github.com>
2023-04-20 18:19:21 -0400

    Dashing // might want to change bc needs update to work.

--- backatit/Assets/Scenes/Player/Scripts/Movement/basicMovement.cs
+++ backatit/Assets/Scenes/Player/Scripts/Movement/basicMovement.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 public class basicMovement : MonoBehaviour
 {
-    public float moveSpeed;
     public Rigidbody2D rigidBody;
     private Vector2 input;
+
+    public float moveSpeed;
+
+    private float activeMoveSpeed;
+    public float dashMoveSpeed;
+    public float dashCoolDown = 0.5f;
+    public float dashLength = 1f;
+    public float dashCoolDownCounter;
+    public float dashCounter;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeMoveSpeed = moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {       
-        
-
+    void Update() {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
 
         input.Normalize();
 
-        rigidBody.velocity = input * moveSpeed;
+        rigidBody.velocity = input * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if(dashCoolDownCounter <= 0 && dashCounter <= 0){
+                activeMoveSpeed = dashMoveSpeed;
+                dashCounter = dashLength;
+            }
+        }
+        
+        if (dashCounter > 0){
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0){
+                activeMoveSpeed = moveSpeed;
+                dashCoolDownCounter = dashCoolDown;
+            }
+        }
+
+        if (dashCoolDownCounter > 0){
+            dashCoolDownCounter -= Time.deltaTime;
+        }
     }
 }
+
