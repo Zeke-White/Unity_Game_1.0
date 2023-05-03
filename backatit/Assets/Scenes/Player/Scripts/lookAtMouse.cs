d8a6f5bc534af51469fd2fa0941805812ec884f3
Zeke-White <zeke.b.white04@gmail.com>
2023-05-03 11:45:27 -0400

    place holder player sprite and look at mouse for player

---
+++ backatit/Assets/Scenes/Player/Scripts/lookAtMouse.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtMouse : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // looking at this for help (https://gamedevbeginner.com/make-an-object-follow-the-mouse-in-unity-in-2d/#look_at_mouse_2D)
+        Vector2 direction = mousePos - transform.position;
+
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        transform.eulerAngles = new Vector3 (0, 0, angle);
+    }
+}
