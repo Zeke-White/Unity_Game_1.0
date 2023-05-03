9c3bdd732ca8648aeb0f1ef09fe3d5b74ef73c40
Zeke-White <zeke.b.white04@gmail.com>
2023-05-03 10:39:23 -0400

    Health script now kill obj its on when hp == 0

--- backatit/Assets/Scenes/Health.cs
+++ backatit/Assets/Scenes/Health.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public int hp = 10;
-    
+    public GameObject Object;
+
 
     public void damage(int dmg){
         hp -= dmg;
         Debug.Log("oww");
+    }
+
+    public void Update() {
+        if (hp <= 0){
+            Destroy(Object);
+        }
     }
 }
