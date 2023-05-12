415c6a25fc90cffc0e126b054fc442aaec6ecb75
Zeke-White <zeke.b.white04@gmail.com>
2023-05-12 09:45:51 -0400

    Reload and ammo

--- backatit/Assets/Main/Player/Scripts/firePoint.cs
+++ backatit/Assets/Main/Player/Scripts/firePoint.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class firePoint : MonoBehaviour
 {
-    public float coolDown;
+    private float coolDown;
     public float coolDownTime = 2f;
     public Rigidbody2D BulletPre;
     public float attackSpeed = 10f;
     public float moveSpeed = 100f;
+
+    public int ammunition = 4; 
+    public int ammunitionUsed;
+    public float reloadTime = 4f;
+    private float reloadTimeHolder;
     
     
 
     private void Update() {
-        
-        
-        if (Input.GetButton("Fire1")){
-            if(coolDown <= Time.time){
-                shoot();
+
+        if(reloadTimeHolder <= Time.time){
+            if (Input.GetButton("Fire1")){
+                if(ammunitionUsed < ammunition){
+                    if(coolDown <= Time.time){
+                        ammunitionUsed++;
+                        shoot();
+                    }
+                }
+                else{
+                    Debug.Log("Reloading");
+                    ammunitionUsed = 0;
+                    reloadTimeHolder = Time.time + reloadTime;
+                }
             }
         }
-
 
     }
     private void shoot(){
